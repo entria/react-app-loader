7083628a14fbc5eb3ea6b9b5b8f0f6b28cc2ce32
João Marins <jgcmarins@gmail.com>
2018-04-01 16:36:39 -0300

    chore(example-readme): improve example and README and fix npmignore

--- example/my-awesome-app/src/App.js
+++ example/my-awesome-app/src/App.js
@@ -1,21 +1,13 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
-// import AnyExternalApp from './externals/AnyExternalApp';
+import AnyExternalApp from './externals/AnyExternalApp';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-        {/*<AnyExternalApp />*/}
+        <AnyExternalApp />
       </div>
     );
   }
